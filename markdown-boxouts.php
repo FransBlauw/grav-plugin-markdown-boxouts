df5813051e14bb51068beac3f515b5c794f53bec
Frans Blauw <github@fransblauw.net>
2021-01-28 16:59:12 +0200

    Enabled rawHtml for the header

--- markdown-boxouts.php
+++ markdown-boxouts.php
@@ -25,59 +25,60 @@ class MarkdownBoxoutsPlugin extends Plugin
         $markdown->addBlockType('!', 'Boxout', true, false);
 
         $markdown->blockBoxout = function($Line) {
-			
+            
             $ConfigClasses = $this->config->get('plugins.markdown-boxouts.boxout_classes');
-			$Classes = array();
-			foreach($ConfigClasses as $Class) {
-				if (!isset($Class['classname'])) continue;
-				$Classes[$Class['classname']] = $Class;
-			}
-			
-			if (empty($Classes)) return;
-			
-            if (preg_match('/^(!)(\(([\w-_]*)\))[ ]+(.*)/', $Line['text'], $matches))
+            $Classes = array();
+            foreach($ConfigClasses as $Class) {
+                if (!isset($Class['classname'])) continue;
+                $Classes[$Class['classname']] = $Class;
+            }
+            
+            if (empty($Classes)) return;
+            
+            if (preg_match('/^(!)(\(([\w\-_]*)\))[ ]+(.*)/', $Line['text'], $matches))
             {
-				$classname = $matches[3];
-				if (!isset($Classes[$classname])) return;
-				$BoxoutTitle = $Classes[$classname]['heading'];
-				$BoxoutIcon = $Classes[$classname]['fontawesome'];
-				
+                $classname = $matches[3];
+                if (!isset($Classes[$classname])) return;
+                $BoxoutTitle = $Classes[$classname]['heading'];
+                $BoxoutIcon = $Classes[$classname]['fontawesome'];
+                
                 $text = $matches[4];
-	
-				$BodyElement = array(
-					'name' => 'div',
-					'attributes' => array(
-                            'class' => 'boxout-body',
+    
+                $BodyElement = array(
+                    'name' => 'div',
+                    'attributes' => array(
+                        'class' => 'boxout-body',
                     ),
-					'handler' => 'lines',
-					'text' => array($text),
-				);
-				
-				$HeaderElement = array(
-					'name' => 'div',
-					'attributes' => array(
-                            'class' => 'boxout-header',
+                    'handler' => 'lines',
+                    'text' => array($text),
+                );
+                
+                $HeaderElement = array(
+                    'name' => 'div',
+                    'allowRawHtmlInSafeMode' => true,
+                    'attributes' => array(
+                        'class' => 'boxout-header',
                     ),
-					'text' => '<i class="fa fa-fw '.$BoxoutIcon.'"></i> '.$BoxoutTitle,
-				);
-				
-				$BoxoutElements[] = $HeaderElement;
-				$BoxoutElements[] = $BodyElement;
-				
-				$Block = array(
-					'element' => array(
-						'name' => 'div',
-						'attributes' => array(
+                    'rawHtml' => '<i class="fa fa-fw '.htmlspecialchars($BoxoutIcon).'"></i> '.$BoxoutTitle,
+                );
+                
+                $BoxoutElements[] = $HeaderElement;
+                $BoxoutElements[] = $BodyElement;
+                
+                $Block = array(
+                    'element' => array(
+                        'name' => 'div',
+                        'attributes' => array(
                             'class' => 'boxout '.$classname,
-						),
-						'handler' => 'elements',
-						'text' => array($HeaderElement,$BodyElement),
-					),
-				);		
+                        ),
+                        'handler' => 'elements',
+                        'text' => array($HeaderElement,$BodyElement),
+                    ),
+                );		
 
                 return $Block;
-			};
-			
+            };
+            
         };
 
         $markdown->blockBoxoutContinue = function($Line, array $Block) {
@@ -93,11 +94,11 @@ class MarkdownBoxoutsPlugin extends Plugin
                 return $Block;
             }
         };
-		
-		$markdown->blockBoxoutComplete = function($Block)
-		{
-			return $Block;
-		};
+        
+        $markdown->blockBoxoutComplete = function($Block)
+        {
+            return $Block;
+        };
     }
 
     public function onTwigSiteVariables()
